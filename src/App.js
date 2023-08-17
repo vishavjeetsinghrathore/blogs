270a8a0c247578cc9935d2a19fbf262f6e15d12e
vishavjeetsinghrathore <2021kucp1030@iiitkota.ac.in>
2023-08-17 13:34:04 +0530

    dnvljsdnv

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import Header from "./components/Header"
+import Blogs from "./components/Blogs"
+import Pageination from "./components/Pageination"
+import {useContext} from "react";
+import {useEffect} from "react";
+import { AppContext } from './context/AppContext';
+import "./App.css";
+
+export default function App() {
+
+   const {fetchBlogPosts} =useContext(AppContext);
+
+   useEffect(()=>{
+
+      fetchBlogPosts();
+   },[])
+  
+   return(
+     <div className="w-full h-full flex flex-col gap-y-1 justify-center items-center">
+        <Header/>
+
+        <Blogs/>
+
+        <Pageination/>
+
+     </div>
+   )
+}
---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+
+import React from 'react'
+
+const Header=()=>{
+
+    return(
+        <div className="w-full border shadow-md py-2 fixed top-0 bg-white">
+            <header className="text-center">
+               <h1 className="text-xl font-bold uppercase">Codehelp Blogs</h1> 
+            </header>
+        </div>
+    )
+}
+
+export default Header
